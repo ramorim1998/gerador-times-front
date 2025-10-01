b6ce3a3dd48367917c5ad1b477a3e5fa59c65a0f
Rafael Amorim <41354049+ramorim1998@users.noreply.github.com>
2025-10-01 18:00:44 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
-  auth0: {
-    domain: 'dev-q8pvey4a13mwd3rg.us.auth0.com', // ← do dashboard Auth0
-    clientId: 'mCUJj3plVjDWB65dH11bKRFOwo8FYvtj', // ← do dashboard Auth0  
-    authorizationParams: {
-      redirect_uri: 'http://localhost:8100' // ← URL local do Ionic
-    }
-  }
+  
 };
 
 /*
