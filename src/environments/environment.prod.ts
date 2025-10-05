35bd6025ffa1b0d90557d2ecc356803cc51acc7c
Rafael Amorim <araujor920@gmail.com>
2025-10-05 16:01:34 -0300

    fix env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gerador-times-back.up.railway.app',
+  apiUrl: 'https://gerador-times-back.up.railway.app/api',
   auth0: {
-      domain: 'dev-q8pvey4a13mwd3rg.us.auth0.com', // ← do dashboard Auth0
-      clientId: 'mCUJj3plVjDWB65dH11bKRFOwo8FYvtj', // ← do dashboard Auth0  
-      authorizationParams: {
-        redirect_uri: 'http://localhost:8100' // ← URL local do Ionic
-      }
+    domain: 'dev-q8pvey4a13mwd3rg.us.auth0.com',
+    clientId: 'mCUJj3plVjDWB65dH11bKRFOwo8FYvtj',
+    authorizationParams: {
+      redirect_uri: 'https://gerador-times.netlify.app'
     }
+  }
 };
